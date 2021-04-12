6e7e60ef5157f640df39f21103e3e7f289cc4ded
Ben <ben@necroteam.eu>
2021-04-12 21:48:23 +0200

    small change

--- app/show/ShowModel.ts
+++ app/show/ShowModel.ts
@@ -11,7 +11,7 @@ interface ShowModel {
     seasons: [
         {
             name: string
-            seasonNumber: number
+            seasonNumber: string
             releaseDat: string
             enabled: boolean
             episodes: [
