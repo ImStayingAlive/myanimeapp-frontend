f2925014b10e149817c0df1de9360b5209118689
Ben <ben@necroteam.eu>
2021-04-26 14:27:00 +0200

    SEO optimaization

--- src/pages/watch-list.tsx
+++ src/pages/watch-list.tsx
@@ -2,15 +2,14 @@ import Navbar from "../layout/common/navbar/Navbar";
 import Footer from "../layout/common/Footer";
 import {useEffect} from "react";
 import userStore from "../app/auth/user/store/UserStore";
-import Router from "next/router";
 import Header from "../layout/common/Header";
 import BrowseShows from "../layout/views/browse/BrowseShows";
 import {observer} from "mobx-react-lite";
 import mainStore from "../layout/common/store/MainStore";
 import Preloader from "../layout/common/PreloaderComponent";
-import {toast} from "react-toastify";
 import SEOModel from "../app/utils/models/SEOModel";
 import SEO from "../layout/common/SEO";
+import LoginPage from "../layout/views/login/LoginPage";
 
 const WatchList = observer(() => {
 
@@ -25,26 +24,28 @@ const WatchList = observer(() => {
     useEffect(() => {
     }, [userStore.watchLater])
 
-    if (!userStore.isLoggedIn) {
-        Router.push({
-            pathname: '/login',
-        }).then(() => {
-            toast("Please login first!")
-        })
-    }
-
     return (
         <div className="min-h-screen w-full">
             <SEO data={SEOData} />
 
             {mainStore.loaded && userStore.isLoggedIn ? (
                 <main>
-                    <Navbar />
-                    <Header title="Watch List" subtitle="For a bit of late night binging." />
+                    {userStore.isLoggedIn ? (
+                        <div>
+                            <Navbar />
+                            <Header title="Watch List" subtitle="For a bit of late night binging." />
 
-                    <BrowseShows dataSet={userStore.watchLater} />
+                            <BrowseShows dataSet={userStore.watchLater} />
 
-                    <Footer />
+                            <Footer />
+                        </div>
+                    ): (
+                        <div>
+                            <Navbar />
+                            <LoginPage />
+                            <Footer />
+                        </div>
+                    )}
                 </main>
             ) : (
                 <Preloader />
