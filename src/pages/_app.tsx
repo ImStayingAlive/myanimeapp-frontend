3d241c20ee5ada8bcbd8e3f62c472551695d22d4
Ben <ben@necroteam.eu>
2021-04-26 12:23:29 +0200

    SEO optimaization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import Preloader from "../layout/common/PreloaderComponent";
 import Toast from "../layout/common/Toast";
 import ShowPopup from "../layout/common/modals/ShowModule/ShowPopup";
 import OpenPopup from "../layout/common/modals/OpenPopup";
+import Head from "next/head";
 
 const App = observer(({Component, pageProps}: AppProps) => {
 
@@ -26,7 +27,24 @@ const App = observer(({Component, pageProps}: AppProps) => {
     }, [userStore.isLoggedIn])
 
     if (!mainStore.loaded) {
-        return <Preloader/>
+        return <>
+                    <Head>
+                        <title>
+                            GroupWatch invite.
+                        </title>
+                        <meta property="og:url" content="https://anime.necrocloud.eu" />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:title" content="You have been invited to a GroupWatch session! Join now." />
+                        <meta name="twitter:card" content="summary" />
+                        <meta
+                            property="og:description"
+                            content=""
+                        />
+                        <meta property="og:site_name" content="MyAnimeApp" />
+                        <meta property="og:image" content="https://i.imgur.com/O4iLnbg.jpeg" />
+                    </Head>
+                    <Preloader/>
+               </>
     }
 
     return (
