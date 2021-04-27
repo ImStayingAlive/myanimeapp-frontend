06a653bba2d7e744f265a51acc5111f8eed5b5d9
Ben <ben@necroteam.eu>
2021-04-27 09:51:08 +0200

    SEO optimaization

--- src/pages/room/[roomName].tsx
+++ src/pages/room/[roomName].tsx
@@ -6,56 +6,74 @@ import SEOModel from "../../app/utils/models/SEOModel";
 import SEO from "../../layout/common/SEO";
 import {useRouter} from "next/router";
 import {useEffect} from "react";
-import {roomStore} from "../../app/room/RoomFacade";
+import {RoomModel, roomStore} from "../../app/room/RoomFacade";
 import {toast} from "react-toastify";
-import {userStore} from "../../app/auth/AuthFacade";
+import {api, userStore} from "../../app/auth/AuthFacade";
 import LoginPage from "../../layout/views/login/LoginPage";
 
-const Room = observer(() => {
+// @ts-ignore
+const Room = observer(({data}) => {
 
     const router = useRouter()
     const {roomName} = router.query
 
+    console.log(data)
+
     useEffect(() => {
-        roomStore.openConnection(roomName, (status) => {
-            if (!status) {
-                router.push("/").then(() => {
-                    toast.error("The room was not found.")
-                })
-            }
-        })
+        if (mainStore.loaded) {
+            roomStore.openConnection(roomName, (status) => {
+                if (!status) {
+                    router.push("/").then(() => {
+                        toast.error("The room was not found.")
+                    })
+                }
+            })
+        }
 
         return function cleanup() {
             roomStore.disconnect()
         }
-    }, [])
+    }, [mainStore.loaded])
 
     const SEOData = new SEOModel(
-        "MyAnimeApp - GroupWatch",
+        data.owner.user.name + " invited you to their GroupWatch!",
         "#1E90FF",
         "You were invited to join a GroupWatch! Click on the invite link, sign in and begin watching with your friends.",
-        "https://i.imgur.com/EZRvRiG.jpeg",
+        data.roomShow.show.background,
         "https://anime.necrocloud.eu"
     )
 
     return (
         <div className="min-h-screen w-full">
-            <SEO data={SEOData} />
+            <SEO data={SEOData}/>
 
             {mainStore.loaded ? (
                 <main>
                     {userStore.isLoggedIn ? (
-                        <RoomView />
-                    ): (
-                        <LoginPage />
+                        <RoomView/>
+                    ) : (
+                        <LoginPage/>
                     )}
                 </main>
             ) : (
-                <Preloader />
+                <Preloader/>
             )}
 
         </div>
     )
 })
+
+// This gets called on every request
+export async function getServerSideProps(context) {
+    let data: RoomModel
+    const {roomName} = context.query
+
+    await api.get("/room/find/" + roomName)
+        .then((response) => {
+            data = response.data
+        })
+
+    return {props: {data}}
+}
 
 export default Room
